26f27bb91c8b629023a570a4eeaf124d6b176223
yangdada2 <429347238@qq.com>
2018-11-20 13:48:43 +0800

    修改user实体类

--- git_project/src/main/java/com/itheima/domain/User.java
+++ git_project/src/main/java/com/itheima/domain/User.java
@@ -3,4 +3,12 @@ package com.itheima.domain;
 public class User {
     private int age;
     private String name;
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getName() {
+        return name;
+    }
 }

0d76d4f0a85366056abf78a8bd39fb9b95edcdd3
yangdada2 <429347238@qq.com>
2018-11-20 15:15:52 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	git_project/src/main/java/com/itheima/domain/User.java

--- git_project/src/main/java/com/itheima/domain/User.java
+++ git_project/src/main/java/com/itheima/domain/User.java
@@ -16,10 +16,6 @@ public class User {
         this.name = name;
     }
 
-    public void setAge(int age) {
-        this.age = age;
-    }
-
     @Override
     public String toString() {
         return "User{" +
